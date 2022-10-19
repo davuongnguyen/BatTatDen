1ee29844303acf4e12eb0d589ffc5a03888e9e59
Đa Vương Nguyên <55850238+davuongnguyen@users.noreply.github.com>
2022-10-19 21:47:22 +0700

    them tinh nang - Hien thi tren man OLED

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
 
+#include <SPI.h>
+#include <Wire.h>
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
+
 #define pinLED D0
 #define pinButton D1
 
-unsigned long time_delay = 0;
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_MOSI D2
+#define OLED_CLK D6
+#define OLED_DC D7
+#define OLED_CS D5
+#define OLED_RESET D4
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+int time_delay = 0;
+byte seconds = 0;
+byte minutes = 0;
 
 unsigned long time_prev = 0;
 
@@ -23,9 +40,14 @@ byte read_button();
 
 /**
  * @brief In ra man hinh LCD
- * 
+ *
  */
 void monitor();
+
+/**
+ * @brief Chuyen doi thoi gian sang phut va giay
+ */
+void cover_times();
 
 void setup()
 {
@@ -36,6 +58,22 @@ void setup()
     digitalWrite(pinLED, HIGH);
 
     pinMode(pinButton, INPUT);
+
+    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+    if (!display.begin(SSD1306_SWITCHCAPVCC))
+    {
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ; // Don't proceed, loop forever
+    }
+
+    // Show initial display buffer contents on the screen --
+    // the library initializes this with an Adafruit splash screen.
+    display.display();
+    delay(2000); // Pause for 2 seconds
+
+    // Clear the buffer
+    display.clearDisplay();
 }
 
 void loop()
@@ -43,7 +81,7 @@ void loop()
     // put your main code here, to run repeatedly:
     if (read_button() == 1)
     {
-        time_delay += 5000;
+        time_delay += 300;
         Serial.println(time_delay);
     }
 
@@ -59,7 +97,7 @@ void loop()
         if ((unsigned long)(millis() - time_prev) > 1000)
         {
 
-            time_delay -= 1000;
+            time_delay -= 1;
             monitor();
 
             // cập nhật lại biến time_prev
@@ -90,5 +128,21 @@ byte read_button()
 
 void monitor()
 {
+    cover_times();
+    display.clearDisplay();
 
+    display.setTextSize(2); // Draw 2X-scale text
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(10, 0);
+    display.print(minutes, 10);
+    display.print(" : ");
+    display.print(seconds, 10);
+    display.display(); // Show initial text
+    delay(100);
+}
+
+void cover_times()
+{
+    minutes = time_delay / 60;
+    seconds = time_delay % 60;
 }
